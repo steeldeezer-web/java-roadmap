f8fd4a960692e6149ca7452b3d3361cdb6e870ba
steeldeezer <steel.deezer@gmail.com>
2026-05-30 14:38:20 +0300

    feat:add solution Task3 for understanding String objects equals

--- src/main/java/ru/steelDeezer/Day3/Task3.java
+++ src/main/java/ru/steelDeezer/Day3/Task3.java
@@ -1,4 +1,21 @@
 package ru.steelDeezer.Day3;
 
 public class Task3 {
+    public static void main(String[] args) {
+        String a = "hello";
+        String b = "hello";
+        String c = new String("hello");
+
+        System.out.println(a == b); //true
+        System.out.println(a == c);//false
+        System.out.println(a.equals(c));//true
+
+        String d = c.intern();
+        System.out.println(a == d);//true
+
+        StringBuilder sb = new StringBuilder("hello");
+        String e = sb.toString();
+        System.out.println(a == e);//false
+    }
+    
 }
