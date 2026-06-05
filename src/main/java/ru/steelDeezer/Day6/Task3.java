0ec0304cd4171384f3d97f5d6be45151d35811c8
steeldeezer-web <steel.deezer@gmail.com>
2026-06-06 02:38:01 +0500

    feat: add Day6.Task3 comparing min-heap and max-heap PriorityQueue

--- src/main/java/ru/steelDeezer/Day6/Task3.java
+++ src/main/java/ru/steelDeezer/Day6/Task3.java
@@ -1,4 +1,25 @@
 package ru.steelDeezer.Day6;
 
+import java.util.*;
+
 public class Task3 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(10,5,20,3,15);
+        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<Integer> queue1 = new PriorityQueue<>();
+        queue1.addAll(list);
+        queue.addAll(list);
+
+        System.out.println("Вывод по убыванию");
+        while (!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+        System.out.println("Вывод по возростанию");
+
+        while (!queue1.isEmpty()){
+            System.out.println(queue1.poll());
+        }
+
+    }
+
 }
