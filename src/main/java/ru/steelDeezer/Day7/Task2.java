18a537db65dbb1604cd8196af462362b86691c82
steeldeezer <steel.deezer@gmail.com>
2026-06-07 14:04:34 +0300

    feat: add Day7.Task2 print hasMaps elements using Iterator

--- src/main/java/ru/steelDeezer/Day7/Task2.java
+++ src/main/java/ru/steelDeezer/Day7/Task2.java
@@ -1,4 +1,26 @@
 package ru.steelDeezer.Day7;
 
+import ru.steelDeezer.Main;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Task2 {
+    public static void main(String[] args) {
+        HashMap<Integer, String> heshMap = new HashMap<>();
+        heshMap.put(1, "one");
+        heshMap.put(2, "two");
+        heshMap.put(3, "three");
+
+        Iterator<Map.Entry<Integer, String>> iterator = heshMap.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<Integer, String> entry = iterator.next();
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+
+        }
+    }
 }
+/*
+feat: add Day7.Task2 print hasMaps elements using Iterator
+ */
