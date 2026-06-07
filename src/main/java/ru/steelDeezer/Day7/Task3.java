437d0ae3d07328b089489759f6df16a4517592cf
steeldeezer <steel.deezer@gmail.com>
2026-06-07 14:05:37 +0300

    feat: add Day7.Task3 print first key and last key using firstEntry and lastEntry

--- src/main/java/ru/steelDeezer/Day7/Task3.java
+++ src/main/java/ru/steelDeezer/Day7/Task3.java
@@ -1,4 +1,23 @@
 package ru.steelDeezer.Day7;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Task3 {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        treeMap.put(3, "three");
+        treeMap.put(1, "one");
+        treeMap.put(4, "four");
+        treeMap.put(2, "two");
+        System.out.println(treeMap);
+
+        Map.Entry<Integer, String> first = treeMap.firstEntry();
+        Map.Entry<Integer, String> last = treeMap.lastEntry();
+        System.out.println(first.getKey());
+        System.out.println(last.getKey());
+
+    }
 }
+"feat: add Day7.Task3 print first key and last key using firstEntry and lastEntry"
